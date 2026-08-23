591b2f98d2ba15cb250eabbaf020937cf6c7c2bd
Nick <tikyikyaw29@gmail.com>
2026-08-23 21:21:28 +0800

    increment level-0, updated banner

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+/**
+ * Entry point for the Duke chatbot application.
+ * For now, the bot only greets the user on startup and says goodbye on exit.
+ */
 public class Duke {
+    private static final String DIVIDER =
+            "____________________________________________________________";
+
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String banner = " _   _  ___ ____ ___  \n"
+                + "| \\ | |/ _ \\___ \\__ \\ \n"
+                + "|  \\| | | |__) | ) |\n"
+                + "| |\\  | |_| / __/ / / \n"
+                + "|_| \\_|\\___/_____|___|\n";
+
+        printGreeting(banner);
+        printFarewell();
+    }
+
+    private static void printGreeting(String banner) {
+        System.out.println(DIVIDER);
         System.out.println(banner);
+        System.out.println("Hello! I'm NICO.");
+        System.out.println("What can I do for you?");
+        System.out.println(DIVIDER);
+    }
+
+    private static void printFarewell() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(DIVIDER);
     }
 }
