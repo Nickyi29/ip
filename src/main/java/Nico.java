4bfbbb42b3bc986e6f88558a373dea65360e17ee
Nick <tikyikyaw29@gmail.com>
2026-08-28 00:06:44 +0800

    Implement <Level-2>

--- src/main/java/Nico.java
+++ src/main/java/Nico.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Nico {
     private static final String DIVIDER =
             "____________________________________________________________";
+    private static final String[] tasks = new String[100];
+    private static int taskCount = 0;
 
     public static void main(String[] args) {
         String banner = " _   _  ___ ____ ___  \n"
@@ -16,6 +18,38 @@ public class Nico {
         printFarewell();
     }
 
+    private static void runCommandLoop() {
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+
+        while (!command.equals("bye")) {
+            System.out.println(DIVIDER);
+            handleCommand(command);
+            System.out.println(DIVIDER);
+            command = scanner.nextLine();
+        }
+    }
+
+    private static void handleCommand(String command) {
+        if (command.equals("list")) {
+            printTaskList();
+        } else {
+            addTask(command);
+        }
+    }
+
+    private static void addTask(String description) {
+        tasks[taskCount] = description;
+        taskCount++;
+        System.out.println("added: " + description);
+    }
+
+    private static void printTaskList() {
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
+        }
+    }
+
     private static void printGreeting(String banner) {
         System.out.println(DIVIDER);
         System.out.println(banner);
@@ -23,39 +57,6 @@ public class Nico {
         System.out.println("What can I do for you?");
         System.out.println(DIVIDER);
     }
-
-    private static void runCommandLoop() {
-        Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
-
-        while(!command.equals("bye")) {
-            System.out.println(DIVIDER);
-            System.out.println(command);
-            System.out.println(DIVIDER);
-            command = scanner.nextLine();
-        }
-    }
-
-    private static void handleCommand(String command){
-        if (command.equals("list")){
-            printTaskList();
-        }
-
-        else if(command.equals("blah")){
-            printBlah();
-        }
-
-    }
-
-
-    private static void printTaskList(){
-        System.out.println("list");
-    }
-
-    private static void printBlah(){
-        System.out.println("blah");
-    }
-
 
     private static void printFarewell() {
         System.out.println("Bye. Hope to see you again soon!");
